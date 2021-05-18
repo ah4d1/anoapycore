f67788ca2371a119e44f048b5ef2392be0c8c7a1
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2021-05-18 13:37:50 +0700

    Update

--- src-release/chart/__init__.py
+++ src-release/chart/__init__.py
@@ -4,30 +4,10 @@ import matplotlib.pyplot as __plt
 import seaborn as __sns
 import multipledispatch as __dispatch
 
-@__dispatch.dispatch(object,str)
-def boxplot (a_data,a_column) :
-    result = __sns.boxplot(x=a_data[a_column])
+from . import boxplot
+from . import correlation
+from . import density
+from . import histogram
+from . import pairplot
+from . import scatterplot
 
-@__dispatch.dispatch(object,str,str)
-def boxplot (a_data,a_column_x,a_column_y) :
-    result = __sns.boxplot(data=a_data,x=a_column_x,y=a_column_y)
-    
-def histogram (a_data,a_column,a_bins='auto',a_color='#0504aa',a_alpha=0.7,a_grid_y=True) :
-    __plt.hist(x=a_data[a_column],bins=a_bins,color=a_color,alpha=a_alpha)
-    __plt.title(a_column)
-    __plt.xlabel('Value')
-    __plt.ylabel('Frequency')
-    if a_grid_y == True :
-        __plt.grid(axis='y',alpha=0.7)
-    __plt.show()
-    
-def scatterplot (a_data,a_column_x,a_column_y,a_column_by="") :
-    """
-    Draw a scatterplot
-    """
-    __sns.set_theme() # apply the default theme
-    if a_column_by == "" :
-        loc_result = __sns.lmplot(data=a_data,x=a_column_x,y=a_column_y)
-    else :
-        loc_result = __sns.lmplot(data=a_data,x=a_column_x,y=a_column_y,col=a_column_by)
-    result = loc_result
--- src-release/ml/__init__.py
+++ src-release/ml/__init__.py
@@ -6,3 +6,5 @@ from . import logistic_regression
 from . import support_vector_classifier
 # regression
 from . import linear_regression
+# clustering
+from . import clustering
