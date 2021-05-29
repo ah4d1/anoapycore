ae5aa48c4b164d46b338a41f2cbd689b1c959d44
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2021-05-29 10:24:12 +0700

    Update 0.1.17

--- src/anoapycore/__init__.py
+++ src/anoapycore/__init__.py
@@ -1,6 +1,7 @@
 from . import data
 from . import chart
 from . import mlearn
+from . import model
 from . import statmodel
 
 from . import __eval
--- src/anoapycore/data/save/__init__.py
+++ src/anoapycore/data/save/__init__.py
@@ -1,5 +1,9 @@
 import pandas as __pd
 
+
+def csv (a_data,a_file,b_separator='~',b_index=False) :
+    a_data.to_csv(a_file,sep=b_separator,index=b_index)
+
 def xls (a_data,a_filename) :
     # create excel writer object
     loc_writer = __pd.ExcelWriter(a_filename)
